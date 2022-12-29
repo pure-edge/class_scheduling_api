d1d079b14e298d469594c9ba48c504efdcec3b02
Stephen Janseen Balo <jeannes.thepsen@gmail.com>
2022-12-29 20:05:10 +0800

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -12,9 +11,8 @@ import { register } from './controllers/auth.js'
 
 dotenv.config()
 const app = express()
-app.use(express.json())
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb' }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 /* routes */
