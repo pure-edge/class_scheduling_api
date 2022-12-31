53e54613d0438e3ff11b3e54142f2f39bef87cfd
Stephen Janseen Balo <jeannes.thepsen@gmail.com>
2022-12-31 10:27:06 +0800

    fixed the user controller and routes

--- models/Curriculum.js
+++ models/Curriculum.js
@@ -5,7 +5,11 @@ const CurriculumSchema = new mongoose.Schema(
     {
         version: { type: String, required: true, unique: true },
         year: { type: Number, required: true },
-        programId: { type: mongoose.Schema.Types.ObjectId, ref: 'Program', required: true },
+        programId: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Program', 
+            required: true 
+        },
         courses: [CourseSchema]
     }, 
     { timestamps: true }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getUser } from '../controllers/users.js'
+import { getUser, getUsers, updateUser, deleteUser } from '../controllers/users.js'
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.get('/', verifyToken, getUsers)
 router.get('/:id', verifyToken, getUser)
+router.put('/:id', verifyToken, updateUser)
+router.delete('/:id', verifyToken, deleteUser)
 
 export default router
